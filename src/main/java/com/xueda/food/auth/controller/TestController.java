4d969e75741e0231f06de59fb7617d91d0acbe84
wansq <cboy868@163.com>
2020-08-07 07:19:37 +0800

    ok

---
+++ src/main/java/com/xueda/food/auth/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.xueda.food.auth.controller;
+
+import com.xueda.food.utils.JsonData;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/auth/test")
+    public JsonData index() {
+        return JsonData.success();
+    }
+    
+}
